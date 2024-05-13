9bae46fd990ff285cb8ac6afed838e0b79d4f63a
darshil929 <darshilt929@gmail.com>
2024-05-13 21:27:08 +0530

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,57 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left, right, arr)
+
+def merge_two_sorted_lists(a, b, arr):
+    len_a = len(a)
+    len_b = len(b)
+    i = j = k = 0
+
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i += 1
+        else:
+            arr[k] = b[j]
+            j += 1
+        k += 1
+
+    while i < len_a:
+        arr[k] = a[i]
+        i += 1
+        k += 1
+
+    while j < len_b:
+        arr[k] = b[j]
+        j += 1
+        k += 1
+
+if __name__ == '__main__':
+    arr = [10, 3, 15, 7, 8, 23, 98, 29]
+    merge_sort(arr)
+    print(f'sorted array: {arr}')
+
+    print("--------------------------------------------")
+
+    print("Test Cases")
+
+    tests = [
+        [3, 7, 9, 11],
+        [25, 22, 21, 10],
+        [29, 15, 28],
+        [],
+        [6]
+    ]
+
+    for arr in tests:
+        merge_sort(arr)
+        print(f"sorted array: {arr}")
